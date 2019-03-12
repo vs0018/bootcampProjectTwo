68f99e83344da773cc65706726c2414284167ac0
Valerie <43964677+vs0018@users.noreply.github.com>
2019-03-12 19:20:43 -0400

    Enhancement/getserverrunning (#23)
    * move rewuired modules around in server.js file
    
    * still trying to get server running
    
    * server still not working
    
    * added Quincy's code back in
    
    * add config to index.js sequelize variable
    
    * fix .env file

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ require("dotenv").config();
 var express = require("express");
 var exphbs = require("express-handlebars");
 
+var PORT = process.env.PORT || 8080;
 var db = require("./models");
 
+// Setting up Express and Middleware
 var app = express();
-var PORT = process.env.PORT || 3000;
 
-// Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
@@ -33,7 +33,7 @@ if (process.env.NODE_ENV === "test") {
   syncOptions.force = true;
 }
 
-// Starting the server, syncing our models ------------------------------------/
+// Starting the server, syncing our models
 db.sequelize.sync(syncOptions).then(function() {
   app.listen(PORT, function() {
     console.log(
