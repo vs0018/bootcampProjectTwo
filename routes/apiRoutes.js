4740565ceec5557ecd443777b79852c7613f3dbf
Valerie S <43964677+vs0018@users.noreply.github.com>
2019-03-12 21:19:01 -0400

    add more API routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,19 +1,33 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  app.get("api/allEvents", function(req, res) {
+  // GET route for returning all events
+  app.get("api/events", function(req, res) {
     db.Event.findAll({}).then(function(dbEvents) {
       res.json(dbEvents);
     });
   });
 
+  // Get route for returning posts of a specific category
+  app.get("/api/events/category/:category", function(req, res) {
+    db.Post.findAll({
+      where: {
+        category: req.params.category
+      }
+    })
+      .then(function(dbPost) {
+        res.json(dbPost);
+      });
+  });
+
+  // POST route for creating a new event
   app.post("api/events", function(req, res) {
     db.Event.create(req.body).then(function(dbEvent) {
       res.json(dbEvent);
     });
   });
 
-  // Create a new user/attendee
+  // POST route to create a new user/attendee
   app.post("/api/attendee", function(req, res) {
     db.Attendee.create(req.body).then(function(dbAttendee) {
       res.json(dbAttendee);
