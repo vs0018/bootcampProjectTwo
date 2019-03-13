9a51f65e4a88303c19f97482efe65b977103cc28
Valerie S <43964677+vs0018@users.noreply.github.com>
2019-03-12 23:43:30 -0400

    added and chained routes based on tables; added comments for clarity

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,42 +1,49 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
+  //EVENT ROUTES
+  // Chained routes for general api/event 
+  app.route("api/events")
   // GET route for returning all events
-  app.get("api/events", function(req, res) {
-    db.Event.findAll({}).then(function(dbEvents) {
-      res.json(dbEvents);
-    });
-  });
-
-  // GET route for returning events of a specific type
-  app.get("/api/events/category/:category", function(req, res) {
-    db.Event.findAll({
-      where: {
-        category: req.params.category
-      }
-    }).then(function(dbPost) {
-      res.json(dbPost);
-    });
-  });
-
+    .get(function (req, res) {
+      db.Event.findAll({}).then(function (dbEvents) {
+        res.json(dbEvents);
+      });
+    })
   // POST route for creating a new event
-  app.post("api/events", function(req, res) {
-    db.Event.create(req.body).then(function(dbEvent) {
-      res.json(dbEvent);
+    .post("api/events", function (req, res) {
+      db.Event.create(req.body).then(function (dbEvent) {
+        res.json(dbEvent);
+      });
     });
-  });
 
-  // POST route to create a new user/attendee
-  app.post("/api/attendee", function(req, res) {
-    db.Attendee.create(req.body).then(function(dbAttendee) {
-      res.json(dbAttendee);
+  // Chained routes by event name
+  app.route("api/events/:name")
+  // GET routes for returning specific events based on search params
+    .get(function (req, res) {
+      db.Event.findAll({
+        where: {
+          name: req.params.name
+        }
+      }).then(function (dbEvent) {
+        res.json(dbEvent);
+      });
     });
-  });
 
   // Delete an event by id
-  app.delete("/api/events/:id", function(req, res) {
-    db.Event.destroy({ where: { id: req.params.id } }).then(function(dbEvent) {
+  app.delete("/api/events/:id", function (req, res) {
+    db.Event.destroy({ where: { id: req.params.id } }).then(function (dbEvent) {
       res.json(dbEvent);
     });
   });
+
+  //ATTENDEE ROUTES
+  // Chained routes for general api/attendee
+  app.route("api/attendee")
+  // POST route to create a new user/attendee
+    .post(function (req, res) {
+      db.Attendee.create(req.body).then(function (dbAttendee) {
+        res.json(dbAttendee);
+      });
+    });
 };
